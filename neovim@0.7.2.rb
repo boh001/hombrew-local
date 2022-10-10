410ab9bdef6a46325e266c23cf02140cd1fed577
kimsang <boh001g@gmail.com>
2022-10-10 16:44:22 +0900

    update

--- neovim@0.7.2.rb
+++ neovim@0.7.2.rb
@@ -1,4 +1,4 @@
-class Neovim < Formula
+class NeovimAT072 < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
   url "https://github.com/neovim/neovim/archive/v0.7.2.tar.gz"
